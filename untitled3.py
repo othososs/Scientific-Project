4370c849c21ba06fbd577e5cd3e59d266510b233
othososs <103260323+othososs@users.noreply.github.com>
2023-10-05 22:04:24 +0200

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,1 +1,1 @@
-=IF(OR(AND(D23="Good (green)"; D24="Good (green)"); AND(D23="Mixed (orange)"; D24="Mixed (orange)"); AND(D23="Bad (red)"; D24="Bad (red)")); D23; "Mixed (orange)")
+=IF(AND(D23="Good (green)"; D24="Good (green)"); "Good (green)"; IF(AND(OR(D23="Good (green)"; D23="Mixed (orange)"); OR(D24="Good (green)"; D24="Mixed (orange)")); "Mixed (orange)"; IF(AND(D23="Bad (red)"; D24="Bad (red)"); "Bad (red)"; "Other")))
