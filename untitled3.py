b70c52b13e1978d01fc5415c7ba30253ac99cacd
othososs <103260323+othososs@users.noreply.github.com>
2023-10-24 17:07:17 +0200

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,1 +1,1 @@
-Je n'ai pas trouvé le fichier CAPA pour l'équipe Pricing dans le répertoire partagé sur Teams. Si tu as le fichier, pourrais-tu me l'envoyer afin que je puisse préparer son fichier de MOS ?
+..
