cbbf337c876f7d27655e86acda544de20c660a8c
othososs <103260323+othososs@users.noreply.github.com>
2023-10-24 16:03:18 +0200

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,3 +1,1 @@
-Tu trouveras deux fichiers de suivi de MOS pour l'équipe S2 et Carac. J'ai déjà récupéré les processus et les activités depuis la capa, il reste à renseigner le reste du tableau. Je peux encore t'aider pour compléter la colonne 'liens vers les fichiers', mais il faut que tu me donnes accès au répertoire. Je reste à ta disposition si tu as des questions.
-
-Cordialement
+,,
