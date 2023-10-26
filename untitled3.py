197f66c4cd55456ce7f606153c28c58dade8c752
othososs <103260323+othososs@users.noreply.github.com>
2023-10-26 15:04:10 +0200

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,1 +1,5 @@
-..
+Bonjour,
+
+J'espère que tu vas bien. Je te contacte pour savoir s'il y a un problème avec les fiches de paie sur la plateforme MyPeople Doc. Normalement, chaque dernière semaine du mois, je reçois ma fiche de paie. Cependant, pour le mois d'octobre, je n'ai rien reçu. Est-ce qu'il y a un problème ou un décalage dans le versement ? Merci d'avance.
+
+Cordialement,
