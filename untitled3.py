5c19d99ad5e765a06d518b84a36687b1ced1e914
othososs <103260323+othososs@users.noreply.github.com>
2023-11-23 21:00:26 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,44 +1,52 @@
-Sub CopierValeurs()
+Sub RemplirFichierDestination()
 
     ' Déclaration des variables
     Dim wsDest As Worksheet
     Dim wsSource As Worksheet
-    Dim fichierSource As String
-    Dim titreCokpit As String
     Dim dateCible As Date
-    Dim valeurCokpit As Double
-    Dim lastColumn As Integer
-    Dim lastRow As Integer
+    Dim derniereligneDest As Long
+    Dim derniereligneSource As Long
+    Dim titreCokpit As String
+    Dim valeurCokpit As Variant
+    Dim colTitreDest As Long
+    Dim colTitreSource As Long
+    Dim i As Long
     
     ' Définir la feuille de destination
     Set wsDest = ThisWorkbook.Sheets("FeuilleDestination")
     
+    ' Déterminer la dernière ligne dans la feuille de destination
+    derniereligneDest = wsDest.Cells(wsDest.Rows.Count, 1).End(xlUp).Row
+    
     ' Boucler à travers les dates dans la première ligne du tableau de destination
-    For Each cel In wsDest.Range("C1:ZZ1")
-        If IsDate(cel.Value) Then
-            ' Récupérer la date cible
-            dateCible = cel.Value
-            
-            ' Boucler à travers les fichiers sources
-            fichierSource = "Cokpit_BASE" & Format(dateCible, "yyyymmdd")
-            Set wsSource = Workbooks.Open("CheminDuDossier\" & fichierSource & ".xlsx").Sheets(1) ' Modifier le chemin selon vos besoins
-            
-            ' Trouver la colonne correspondante au titre du cokpit dans le fichier source
-            lastColumn = wsSource.Cells(1, wsSource.Columns.Count).End(xlToLeft).Column
-            colTitre = Application.Match("Titre cokpit", wsSource.Rows(1), 0)
+    For i = 4 To wsDest.Cells(1, wsDest.Columns.Count).End(xlToLeft).Column
+        ' Récupérer la date cible
+        dateCible = wsDest.Cells(1, i).Value
+        
+        ' Ouvrir le fichier source correspondant
+        Set wsSource = Workbooks.Open("CheminDuDossier\Cokpit_BASE" & Format(dateCible, "yyyymmdd") & ".xlsx").Sheets(1)
+        
+        ' Trouver la colonne correspondante au titre du cockpit dans le fichier source
+        colTitreSource = Application.Match("Titre cockpit", wsSource.Rows(1), 0)
+        
+        ' Boucler à travers les lignes dans le fichier de destination
+        For j = 2 To derniereligneDest
+            ' Récupérer le titre du cockpit à partir du fichier de destination
+            titreCokpit = wsDest.Cells(j, 2).Value
             
             ' Trouver la ligne correspondante au titre dans le fichier source
-            lastRow = wsSource.Cells(wsSource.Rows.Count, colTitre).End(xlUp).Row
-            ligneTitre = Application.Match(wsDest.Cells(1, cel.Column).Value, wsSource.Range(wsSource.Cells(1, colTitre), wsSource.Cells(lastRow, colTitre)), 0)
+            derniereligneSource = wsSource.Cells(wsSource.Rows.Count, colTitreSource).End(xlUp).Row
+            ligneTitreSource = Application.Match(titreCokpit, wsSource.Range(wsSource.Cells(1, colTitreSource), wsSource.Cells(derniereligneSource, colTitreSource)), 0)
             
-            ' Copier la valeur du cokpit dans le fichier destination
-            valeurCokpit = wsSource.Cells(ligneTitre, colTitre + 5).Value ' Colonne G dans l'exemple
-            
-            wsDest.Cells(2, cel.Column).Value = valeurCokpit
-            
-            ' Fermer le fichier source
-            Workbooks(fichierSource).Close SaveChanges:=False
-        End If
-    Next cel
+            ' Copier la valeur du cockpit dans le fichier destination
+            If Not IsError(ligneTitreSource) Then
+                valeurCokpit = wsSource.Cells(ligneTitreSource, colTitreSource + 5).Value ' Colonne G dans l'exemple
+                wsDest.Cells(j, i).Value = valeurCokpit
+            End If
+        Next j
+        
+        ' Fermer le fichier source
+        Workbooks("Cokpit_BASE" & Format(dateCible, "yyyymmdd") & ".xlsx").Close SaveChanges:=False
+    Next i
 
 End Sub
