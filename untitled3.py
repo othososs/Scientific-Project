98970948933fec8f887a6505cfc0ba3f0f4ae7bd
othososs <103260323+othososs@users.noreply.github.com>
2023-11-23 18:12:10 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,21 +1,2 @@
+Etes-vous disponible pour la soirée du 19 décembre à partir de 18h ?
 
-Bien sûr, voici un exemple simple de formulaire pour recueillir les préférences des collaborateurs pour la soirée de fin d'année :
-
-Sondage sur la Soirée de Fin d'Année
-
-Chers collaborateurs,
-
-Nous sommes ravis de vous inviter à notre soirée de fin d'année qui aura lieu le 19 décembre à partir de 18h. Afin de rendre cet événement mémorable, nous aimerions recueillir vos préférences pour choisir l'activité et le lieu qui correspondent le mieux à vos attentes.
-
-1. Vos coordonnées :
-Nom : ________________
-Prénom : ________________
-Service/Département : ________________
-Email : ________________
-Téléphone : ________________
-
-2. Préférences pour la soirée :
-Merci de sélectionner l'option de votre choix pour l'activité et le restaurant.
-
-a. Restaurant 1 : Apero dinatoire et boissons bar au choix et Billard/baby foot, musique
-b. Restaurant 2 : Escape Game et Buffet
