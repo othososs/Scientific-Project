fd47939982eedff730eda27dc57c5d545b279856
othososs <103260323+othososs@users.noreply.github.com>
2023-11-24 10:25:05 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,67 +1,41 @@
-Sub DeplacerMois()
-
-    Dim debutColonne As Integer
-    Dim finColonne As Integer
-    Dim dateCell As Range
-    Dim moisActuel As Integer
-    Dim anneeActuelle As Integer
-
-    ' Spécifiez la plage des colonnes contenant les dates
-    debutColonne = 5 ' Colonne E
-    finColonne = 26 ' Colonne Z
-
-    ' Récupérez la date de la première cellule de la plage
-    Set dateCell = Cells(1, debutColonne)
-
-    ' Obtenez le mois actuel et l'année
-    moisActuel = Month(dateCell.Value)
-    anneeActuelle = Year(dateCell.Value)
-
-    ' Demandez à l'utilisateur s'il veut déplacer vers le mois suivant ou précédent
-    Dim choix As Integer
-    choix = MsgBox("Voulez-vous déplacer vers le mois suivant (Oui) ou le mois précédent (Non)?", vbYesNo)
-
-    ' Déplacez les dates en conséquence
-    If choix = vbYes Then
-        moisActuel = moisActuel + 1
-        If moisActuel > 12 Then
-            moisActuel = 1
-            anneeActuelle = anneeActuelle + 1
-        End If
-    Else
-        moisActuel = moisActuel - 1
-        If moisActuel < 1 Then
-            moisActuel = 12
-            anneeActuelle = anneeActuelle - 1
-        End If
-    End If
-
-    ' Mettez à jour les dates dans les colonnes spécifiées
-    For i = debutColonne To finColonne
-        Set dateCell = Cells(1, i)
-        dateCell.Value = GetProchaineDateValide(dateCell.Value, choix)
-    Next i
-
-End Sub
-
-Function GetProchaineDateValide(dateOrigine As Date, sens As Integer) As Date
-    ' Fonction pour obtenir la prochaine date valide (en excluant les week-ends)
-
+Sub ChangerMois(versMoisSuivant As Boolean)
+    Dim feuille As Worksheet
+    Dim colonneDebut As Integer
+    Dim colonneFin As Integer
+    Dim ligneDebut As Integer
+    Dim ligneFin As Integer
+    Dim celluleDate As Range
     Dim nouvelleDate As Date
-    Dim joursAajouter As Integer
-    nouvelleDate = dateOrigine
     
-    ' Spécifie le nombre de jours à ajouter ou soustraire en fonction du sens
-    If sens = vbYes Then
-        joursAajouter = 1
+    ' Spécifiez la feuille de calcul
+    Set feuille = ThisWorkbook.Sheets("NomDeVotreFeuille") ' Remplacez "NomDeVotreFeuille" par le nom de votre feuille
+    
+    ' Spécifiez la plage de colonnes et de lignes
+    colonneDebut = 5 ' Colonne E
+    colonneFin = 34 ' Colonne AH
+    ligneDebut = 2 ' 2ème ligne
+    ligneFin = 30 ' 30ème ligne
+    
+    ' Trouver la cellule contenant la date actuelle (la première date dans la plage spécifiée)
+    Set celluleDate = feuille.Cells(ligneDebut, colonneDebut)
+    
+    ' Récupérer la date actuelle
+    Dim dateActuelle As Date
+    dateActuelle = celluleDate.Value
+    
+    ' Calculer la nouvelle date en ajoutant ou en soustrayant un mois
+    If versMoisSuivant Then
+        nouvelleDate = DateAdd("m", 1, dateActuelle)
     Else
-        joursAajouter = -1
+        nouvelleDate = DateAdd("m", -1, dateActuelle)
     End If
-
-    ' Ajoute ou soustrait les jours et ajuste si nécessaire pour éviter les week-ends
-    Do
-        nouvelleDate = DateAdd("d", joursAajouter, nouvelleDate)
-    Loop While Weekday(nouvelleDate, vbMonday) > 5 ' Exclut les samedis et dimanches
-
-    GetProchaineDateValide = nouvelleDate
-End Function
+    
+    ' Mettre à jour la première cellule de date
+    celluleDate.Value = nouvelleDate
+    
+    ' Mettre à jour les valeurs en dessous de la date dans la plage spécifiée
+    Dim i As Integer
+    For i = ligneDebut + 1 To ligneFin
+        feuille.Cells(i, colonneDebut).FormulaR1C1 = "=EDATE(R[-1]C,1)"
+    Next i
+End Sub
