72602f3e7b76614cf8605a2f66d2c16a85d8f57e
othososs <103260323+othososs@users.noreply.github.com>
2023-11-24 09:07:54 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -3,9 +3,9 @@ Sub DeplacerMois()
     Dim debutColonne As Integer
     Dim finColonne As Integer
     Dim dateCell As Range
-    Dim jourSemaine As Integer
     Dim moisActuel As Integer
     Dim anneeActuelle As Integer
+    Dim limiteTentatives As Integer
 
     ' Spécifiez la plage des colonnes contenant les dates
     debutColonne = 5 ' Colonne E
@@ -37,24 +37,37 @@ Sub DeplacerMois()
         End If
     End If
 
+    ' Limite le nombre de tentatives pour éviter une boucle infinie
+    limiteTentatives = 100
+
     ' Mettez à jour les dates dans les colonnes spécifiées
     For i = debutColonne To finColonne
         Set dateCell = Cells(1, i)
-        dateCell.Value = GetProchaineDateValide(dateCell.Value, choix)
+        dateCell.Value = GetProchaineDateValide(dateCell.Value, choix, limiteTentatives)
     Next i
 
 End Sub
 
-Function GetProchaineDateValide(dateOrigine As Date, sens As Integer) As Date
+Function GetProchaineDateValide(dateOrigine As Date, sens As Integer, limiteTentatives As Integer) As Date
     ' Fonction pour obtenir la prochaine date valide (en excluant les week-ends)
     Dim nouvelleDate As Date
+    Dim tentatives As Integer
     nouvelleDate = dateOrigine
+    tentatives = 0
+
     Do
         If sens = vbYes Then
             nouvelleDate = DateAdd("d", 1, nouvelleDate)
         Else
             nouvelleDate = DateAdd("d", -1, nouvelleDate)
         End If
+
+        ' Évite une boucle infinie
+        tentatives = tentatives + 1
+        If tentatives > limiteTentatives Then
+            Exit Do
+        End If
+
     Loop While Weekday(nouvelleDate, vbMonday) > 5 ' Exclut les samedis et dimanches
 
     GetProchaineDateValide = nouvelleDate
