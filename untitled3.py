fc21717c04fcec150be19a696a452e9a0acb2657
othososs <103260323+othososs@users.noreply.github.com>
2023-11-24 16:10:20 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,32 +1,51 @@
-Sub ChangerDatesEtValeursSansWeekend()
-    ' Demander à l'utilisateur d'entrer le mois
-    Dim mois As String
-    mois = InputBox("Entrez le mois (MM/YYYY) :", "Changer les dates")
+Sub RemplirValeurs()
 
-    ' Vérifier si l'utilisateur a annulé
-    If mois = "" Then
-        Exit Sub
-    End If
+    Dim ws As Worksheet
+    Dim dateCell As Range
+    Dim folderPath As String, monthFolder As String
+    Dim fileName As String
+    Dim lastRow As Long
+    Dim rowCount As Long
+    
+    ' Référence à la feuille de calcul
+    Set ws = ThisWorkbook.Sheets("NomDeVotreFeuille")
 
-    ' Déterminer le premier jour du mois
-    Dim premierJour As Date
-    premierJour = DateValue("01/" & mois)
+    ' Chemin du dossier racine
+    folderPath = "C:\Dossier\down\"
 
-    ' Boucler à travers les colonnes de E à AH
-    Dim col As Integer
-    For col = 5 To 34 ' Correspond à la plage E1:AH1
-        ' Trouver le premier jour ouvrable du mois
-        While Weekday(premierJour, vbMonday) > 5 ' Si c'est un week-end
-            premierJour = premierJour + 1 ' Passer au jour suivant
-        Wend
+    ' Parcourir les cellules de la ligne 1 de E à AA
+    For Each dateCell In ws.Range("E1:AA1")
+        ' Convertir la date en format "mm - Mois"
+        monthFolder = Format(dateCell.Value, "mm - Mmmm")
+        
+        ' Construire le chemin complet du dossier
+        folderPath = folderPath & monthFolder & "\"
 
-        ' Changer la date dans la cellule en fonction du mois fourni par l'utilisateur
-        Cells(1, col).Value = premierJour
+        ' Parcourir les fichiers dans le dossier
+        fileName = Dir(folderPath & "COU2 Places - Circuit du " & Format(dateCell.Value, "ddmmyy") & ".xls")
+        
+        ' Vérifier si le fichier existe
+        If Len(fileName) > 0 Then
+            ' Ouvrir le classeur de travail
+            Workbooks.Open (folderPath & fileName)
+            
+            ' Trouver le nombre de lignes dans la colonne X, à partir de la ligne 3
+            lastRow = Cells(Rows.Count, "X").End(xlUp).Row
+            ' Filtrer et compter les lignes sans "Pas intervention"
+            rowCount = Application.WorksheetFunction.CountIfs(ws.Range("X3:X" & lastRow), "<>Pas intervention")
+            
+            ' Fermer le classeur de travail
+            ActiveWorkbook.Close
+            
+            ' Mettre la valeur dans la cellule correspondante sur la feuille d'origine
+            ws.Cells(28, dateCell.Column).Value = rowCount
+        Else
+            ' Si le fichier n'existe pas, afficher un message d'erreur
+            MsgBox "Le fichier pour la date " & Format(dateCell.Value, "dd/mm/yyyy") & " n'a pas été trouvé.", vbExclamation
+        End If
+        
+        ' Réinitialiser le chemin du dossier racine pour la prochaine itération
+        folderPath = "C:\Dossier\down\"
+    Next dateCell
 
-        ' Effacer les valeurs dans les cellules de la colonne correspondante (ligne 2 à 30)
-        Columns(col).Range("A2:A30").ClearContents
-
-        ' Passer au jour ouvrable suivant
-        premierJour = premierJour + 1
-    Next col
 End Sub
