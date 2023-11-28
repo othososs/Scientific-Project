c003529c09aa4e9e57ddb2c9549e6714de07b7ef
othososs <103260323+othososs@users.noreply.github.com>
2023-11-28 21:39:27 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,60 +1,45 @@
-Sub TraitementDonnees()
-
-    ' Déclaration des variables
+Sub MiseAJourFichiers()
     Dim ws As Worksheet
-    Dim cheminRepertoire As String
-    Dim dossierMois As String
-    Dim fichier As String
-    Dim dateFichier As Date
-    Dim wb As Workbook
-    Dim wsFichier As Worksheet
-    Dim lastRow As Long
-    Dim bloquants As Long
-    Dim importants As Long
+    Dim col As Range
+    Dim dateCol As Range
+    Dim sourceFilePath As String
+    Dim currentDate As Date
+    Dim sumValue As Double
     
-    ' Référence à la feuille de travail actuelle
-    Set ws = ThisWorkbook.Sheets("VotreFeuille")
-    
-    ' Récupérer les dates dans la plage E1:AA1
-    For Each cell In ws.Range("E1:AA1")
-        If IsDate(cell.Value) Then
-            ' Récupérer la date
-            dateFichier = cell.Value
-            
-            ' Construire le chemin du répertoire
-            cheminRepertoire = "C:\Votre\Chemin\Repertoire\" ' Mettez votre chemin de répertoire ici
-            dossierMois = Format(dateFichier, "mm-mmmm")
-            cheminRepertoire = cheminRepertoire & dossierMois & "\"
-            
-            ' Ouvrir le classeur dans le répertoire
-            fichier = Dir(cheminRepertoire & "ecacouQP_D" & Format(dateFichier, "yymmdd") & "*.xlsx")
-            
-            Do While fichier <> ""
-                ' Ouvrir le classeur
-                Set wb = Workbooks.Open(cheminRepertoire & fichier)
-                
-                ' Utiliser la première feuille du classeur ouvert
-                Set wsFichier = wb.Sheets(1)
-                
-                ' Filtrer la colonne AE
-                wsFichier.AutoFilterMode = False ' Supprimer les filtres existants
-                wsFichier.Range("AE:AE").AutoFilter Field:=1, Criteria1:="<>Pas d'intervention auto"
-                
-                ' Compter le nombre de lignes Bloquants et Importants
-                bloquants = Application.WorksheetFunction.CountIf(wsFichier.Range("A:A"), "Bloquant")
-                importants = Application.WorksheetFunction.CountIf(wsFichier.Range("A:A"), "Important")
-                
-                ' Fermer le classeur sans sauvegarder les modifications
-                wb.Close SaveChanges:=False
-                
-                ' Mettre à jour les données sur la feuille principale
-                ws.Cells(29, ws.Columns(dateFichier).Column).Value = bloquants
-                ws.Cells(30, ws.Columns(dateFichier).Column).Value = importants
-                
-                ' Trouver le prochain fichier
-                fichier = Dir
-            Loop
-        End If
-    Next cell
+    ' Spécifiez ici le répertoire où se trouvent vos fichiers sources
+    Const sourceDirectory As String = "C:\CheminetDeVotreDossier\"
 
+    ' Spécifiez ici la ligne où se trouvent les dates et la ligne où vous souhaitez placer les sommes
+    Const datesRow As Integer = 1
+    Const sumRow As Integer = 18
+
+    ' Obtenez la feuille de calcul active (vous pouvez également spécifier une feuille spécifique)
+    Set ws = ActiveSheet
+
+    ' Boucle sur les colonnes de la première ligne (E à AA)
+    For Each col In ws.Range("E:AA").Columns
+        ' Récupère la date de la colonne actuelle
+        currentDate = ws.Cells(datesRow, col.Column).Value
+
+        ' Génère le nom de fichier source
+        sourceFilePath = sourceDirectory & "Cokpit_BASE" & Format(currentDate, "YYYYMMDD") & ".xlsx"
+
+        ' Vérifie si le fichier source existe
+        If Dir(sourceFilePath) <> "" Then
+            ' Ouvre le fichier source
+            Workbooks.Open sourceFilePath
+
+            ' Fait la somme des lignes 10 à 12 de la colonne G
+            sumValue = Application.WorksheetFunction.Sum(Range("G10:G12"))
+
+            ' Place la somme dans le fichier d'origine à la colonne de la date correspondante et à la ligne 18
+            ws.Cells(sumRow, col.Column).Value = sumValue
+
+            ' Ferme le fichier source sans sauvegarder les modifications
+            ActiveWorkbook.Close False
+        Else
+            ' Affiche un message d'avertissement si le fichier source n'est pas trouvé
+            MsgBox "Le fichier source pour la date " & Format(currentDate, "YYYYMMDD") & " n'a pas été trouvé.", vbExclamation
+        End If
+    Next col
 End Sub
