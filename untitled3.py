7aee9bc695a6abbf3fa56dde7be9d24141983a82
othososs <103260323+othososs@users.noreply.github.com>
2023-11-28 22:32:07 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -3,13 +3,13 @@ Function ExtractDateFromFileName(fileName As String) As String
     Dim startPos As Integer
     Dim endPos As Integer
     
-    startPos = InStr(fileName, "D")
+    startPos = InStr(fileName, "Cr_maj_valeurs_") + Len("Cr_maj_valeurs_") + 1
+    endPos = startPos + 5 ' La date est toujours composée de 6 caractères après "Cr_maj_valeurs_"
     
     If startPos > 0 Then
-        endPos = startPos + 6 ' La date est toujours composée de 6 caractères après le "D"
-        ExtractDateFromFileName = Mid(fileName, startPos + 1, 6)
+        ExtractDateFromFileName = Mid(fileName, startPos, 6)
     Else
-        ' Retourne une chaîne vide si "D" n'est pas trouvé
+        ' Retourne une chaîne vide si "Cr_maj_valeurs_" n'est pas trouvé
         ExtractDateFromFileName = ""
     End If
 End Function
