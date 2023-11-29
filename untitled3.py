d25ac29a32fbd74103253df7d80e73d77b31d6cb
othososs <103260323+othososs@users.noreply.github.com>
2023-11-29 21:24:55 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,69 +1,27 @@
-Sub ValeurSuivi()
-    Dim ws As Worksheet
-    Dim col As Range
-    Dim currentDate As String
-    Dim sourceFilePath As String
-    Dim rowCount As Long
-    
-    ' Spécifiez ici le répertoire où se trouvent vos fichiers sources
-    Const sourceDirectory As String = "C:\CheminetDeVotreDossier\"
+Sub FiltrerEtCompter()
+    Dim sourceWorkbook As Workbook
+    Dim sourceWorksheet As Worksheet
+    Dim lastRow As Long
+    Dim countAControler As Long
 
-    ' Obtenez la feuille de calcul active (vous pouvez également spécifier une feuille spécifique)
-    Set ws = ActiveSheet
+    ' Assurez-vous que le classeur est ouvert ou ouvrez-le
+    Set sourceWorkbook = Workbooks("SourceWorkbook.xlsx")
 
-    ' Boucle sur les colonnes de la première ligne (E à Z)
-    For Each col In ws.Range("E:Z").Columns
-        ' Récupère la date de la colonne actuelle
-        currentDate = ExtractDateFromFileName(ws.Cells(1, col.Column).Value)
-        
-        ' Génère le nom de fichier source
-        sourceFilePath = sourceDirectory & "Fichier_D" & currentDate & "_T*"
+    ' Spécifiez la feuille de calcul à l'intérieur du classeur
+    Set sourceWorksheet = sourceWorkbook.Sheets("NomDeVotreFeuille")
 
-        ' Vérifie si le fichier source existe
-        If FileExists(sourceFilePath) Then
-            ' Ouvre le fichier source
-            Workbooks.Open sourceFilePath
+    ' Obtenez la dernière ligne dans la colonne E de cette feuille
+    lastRow = sourceWorksheet.Cells(sourceWorksheet.Rows.Count, "E").End(xlUp).Row
 
-            ' Applique un filtre dans la colonne E pour sélectionner "A controler"
-            ws.Columns("E").AutoFilter Field:=1, Criteria1:="A controler"
+    ' Appliquer le filtre
+    sourceWorksheet.Range("E1:E" & lastRow).AutoFilter Field:=1, Criteria1:="A controler"
 
-            ' Compte le nombre de lignes filtrées (à l'exception de la ligne d'en-tête)
-            rowCount = ws.AutoFilter.Range.Columns(1).SpecialCells(xlCellTypeVisible).Cells.Count - 1
+    ' Compter le nombre de lignes filtrées
+    countAControler = sourceWorksheet.AutoFilter.Range.Columns(1).SpecialCells(xlCellTypeVisible).Cells.Count - 1
 
-            ' Place le nombre de lignes dans le fichier d'origine à la 2ème ligne de la date correspondante
-            ws.Cells(2, col.Column).Value = rowCount
+    ' Afficher le résultat
+    MsgBox "Nombre de lignes à contrôler : " & countAControler
 
-            ' Désactive le filtre
-            ws.AutoFilterMode = False
-
-            ' Ferme le fichier source sans sauvegarder les modifications
-            ActiveWorkbook.Close False
-        Else
-            ' Affiche un message d'avertissement si le fichier source n'est pas trouvé
-            MsgBox "Le fichier source pour la date " & currentDate & " n'a pas été trouvé.", vbExclamation
-        End If
-    Next col
+    ' Supprimer le filtre
+    sourceWorksheet.AutoFilterMode = False
 End Sub
-
-Function ExtractDateFromFileName(fileName As String) As String
-    ' Extrait la date du nom de fichier au format "YYYYMMDD"
-    Dim startPos As Integer
-    Dim endPos As Integer
-    
-    startPos = InStr(fileName, "D") + 1
-    endPos = InStr(startPos, fileName, "_") - 1
-    
-    If startPos > 0 Then
-        ExtractDateFromFileName = Mid(fileName, startPos, endPos - startPos + 1)
-    Else
-        ' Retourne une chaîne vide si "D" n'est pas trouvé
-        ExtractDateFromFileName = ""
-    End If
-End Function
-
-Function FileExists(filePath As String) As Boolean
-    ' Vérifie si le fichier existe
-    On Error Resume Next
-    FileExists = (Len(Dir(filePath)) > 0)
-    On Error GoTo 0
-End Function
