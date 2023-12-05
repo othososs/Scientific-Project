714160849dde7e0dc0cfa6677f81ed516d05c0b6
othososs <103260323+othososs@users.noreply.github.com>
2023-12-05 09:14:23 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,50 +1,36 @@
-Sub MiseAJourDonnees()
-    Dim ws As Worksheet
-    Dim col As Range
-    Dim currentDate As Date
-    Dim sourceFilePath As String
-    Dim rowCountBloquant As Long
-    Dim rowCountImportant As Long
+Sub CreerCalendrier()
+    Dim dateDebut As Date
+    Dim dateFin As Date
+    Dim mois As Integer
+    Dim annee As Integer
+    Dim jourActuel As Date
+    Dim colActuelle As Integer
     
-    ' Obtenez la feuille de calcul active (vous pouvez également spécifier une feuille spécifique)
-    Set ws = ActiveSheet
-
-    ' Boucle sur les colonnes de la première ligne (E à AA)
-    For Each col In ws.Range("E:AA").Columns
-        ' Récupère la date de la colonne actuelle
-        currentDate = ws.Cells(1, col.Column).Value
-        
-        ' Génère le nom de fichier source
-        sourceFilePath = "Chemin\vers\votre\dossier\" & "NomDuFichier_" & Format(currentDate, "YYYYMMDD") & ".xlsx"
-
-        ' Vérifie si le fichier source existe
-        If FileExists(sourceFilePath) Then
-            ' Ouvre le fichier source
-            Workbooks.Open sourceFilePath
-
-            ' Applique un filtre dans la colonne AE pour sélectionner tous sauf 'Pas intervention'
-            ws.Columns("AE").AutoFilter Field:=1, Criteria1:="<>Pas intervention"
-
-            ' Compte le nombre de lignes Bloquantes et importantes dans la colonne A
-            rowCountBloquant = Application.WorksheetFunction.CountIf(ws.Columns("A"), "Bloquant")
-            rowCountImportant = Application.WorksheetFunction.CountIf(ws.Columns("A"), "Important")
-
-            ' Ferme le fichier source sans sauvegarder les modifications
-            ActiveWorkbook.Close False
-
-            ' Met à jour les données sur la feuille principale
-            ws.Cells(29, col.Column).Value = rowCountBloquant
-            ws.Cells(30, col.Column).Value = rowCountImportant
-        Else
-            ' Affiche un message d'avertissement si le fichier source n'est pas trouvé
-            MsgBox "Le fichier source pour la date " & Format(currentDate, "YYYYMMDD") & " n'a pas été trouvé.", vbExclamation
+    ' Demander à l'utilisateur de saisir le mois et l'année
+    mois = InputBox("Entrez le mois (1-12):", "Mois")
+    annee = InputBox("Entrez l'année:", "Année")
+    
+    ' Vérifier si les valeurs saisies sont valides
+    If mois < 1 Or mois > 12 Then
+        MsgBox "Mois invalide. Veuillez entrer un mois entre 1 et 12.", vbExclamation
+        Exit Sub
+    End If
+    
+    ' Définir la date de début comme étant le premier jour du mois spécifié
+    dateDebut = DateSerial(annee, mois, 1)
+    
+    ' Trouver le dernier jour du mois
+    dateFin = DateSerial(annee, mois + 1, 1) - 1
+    
+    ' Boucler à travers les jours du mois
+    For jourActuel = dateDebut To dateFin
+        ' Vérifier si le jour est un jour de semaine (du lundi au vendredi)
+        If Weekday(jourActuel, vbMonday) >= 2 And Weekday(jourActuel, vbMonday) <= 6 Then
+            ' Trouver la prochaine colonne vide à partir de la colonne E
+            colActuelle = Cells(1, Columns.Count).End(xlToLeft).Column + 1
+            
+            ' Mettre à jour la cellule avec la nouvelle date
+            Cells(1, colActuelle).Value = jourActuel
         End If
-    Next col
+    Next jourActuel
 End Sub
-
-Function FileExists(filePath As String) As Boolean
-    ' Vérifie si le fichier existe
-    On Error Resume Next
-    FileExists = (Len(Dir(filePath)) > 0)
-    On Error GoTo 0
-End Function
