8d2c45dcbc337cd74b490c1751f1cd5597ea118b
othososs <103260323+othososs@users.noreply.github.com>
2023-12-19 17:23:45 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,19 +1,21 @@
-VotreMesure =
-VAR TotalCol24 =
-    CALCULATE(
-        SUMX(
-            FILTER(
-                'NomDeVotreTable',
-                'NomDeVotreTable'[Colonne95] = "FR34" ||
-                'NomDeVotreTable'[Colonne95] = "FR53" ||
-                'NomDeVotreTable'[Colonne95] = "FR134" ||
-                'NomDeVotreTable'[Colonne95] = "FR64"
-            ),
-            'NomDeVotreTable'[Colonne24]
-        )
-    )
-
-VAR ValeurCol5 = VALUES('NomDeVotreTable'[Colonne5])
-
-RETURN
-DIVIDE(TotalCol24, ValeurCol5)
+Sub SauvegarderFeuille()
+    Dim chemin As String
+    Dim nomFichier As String
+    
+    ' Spécifiez le chemin du répertoire où vous souhaitez sauvegarder la feuille
+    chemin = "C:\Chemin\vers\le\répertoire\"
+    
+    ' Spécifiez le nom du fichier
+    nomFichier = "NomDuFichier.xlsx"
+    
+    ' Vérifiez si le répertoire existe, sinon créez-le
+    If Dir(chemin, vbDirectory) = "" Then
+        MkDir chemin
+    End If
+    
+    ' Sauvegarde de la feuille dans le répertoire spécifié avec le nom spécifié
+    ThisWorkbook.SaveAs chemin & nomFichier
+    
+    ' Affiche un message indiquant que la sauvegarde a été effectuée avec succès
+    MsgBox "Feuille sauvegardée avec succès dans : " & chemin & nomFichier, vbInformation
+End Sub
