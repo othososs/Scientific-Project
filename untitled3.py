1da4cd6e38f48e095448e97e5c2014631e28b718
othososs <103260323+othososs@users.noreply.github.com>
2024-02-22 11:31:03 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,28 +1,26 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 
-# Supposons que vos données soient dans un DataFrame Pandas appelé 'data'
-# Assurez-vous que les colonnes sont correctement nommées
-# (vous pouvez adapter les noms de colonnes selon vos données)
+On average throughout the year 2023, the number of current account openings hovers around 39,142, reaching a peak of 4,500 in January 2022, followed by a notable decline in February to approximately 2,910
 
-# Exemple de données (remplacez cela par vos propres données)
-data = pd.DataFrame({
-    'Category': ['A', 'B', 'C'],
-    'Médiane': [10, 15, 20],
-    'Moyenne': [12, 18, 22],
-    'Écart-type': [2, 3, 4],
-    'Min': [8, 12, 18],
-    'Max': [15, 22, 25]
-})
 
-# Reformater les données pour les adapter à un boxplot
-data_melted = pd.melt(data, id_vars=['Category'], value_vars=['Médiane', 'Moyenne', 'Écart-type', 'Min', 'Max'])
+The number of approved accounts remains almost constant throughout the year, with an average of 590. This is in contrast to pending accounts, where the average stands at 160.
 
-# Créer un boxplot
-sns.set(style="whitegrid")
-plt.figure(figsize=(10, 6))
-sns.boxplot(x="variable", y="value", hue="Category", data=data_melted)
+Among all the current account openings, averaging 39,000, it appears that nearly 12% express interest in additional current account products.
 
-plt.title('Résumé statistique')
-plt.show()
+During the initial half-year period, a notable disparity was evident between the quantities of unselected and selected saving accounts, with the former exhibiting a considerably higher count. Nevertheless, in the latter half-year, a discernible upswing is observed, marked by an average increase of 400 in the selected saving accounts.
+
+
+Clients reaching the final onboarding step, "Onboarding final CC" represent an average of 13% of the total number of visitors.
+
+
+The number of users rejected by KYC criteria remains constant throughout the year, hovering at around 0.20%.
+
+
+In the year 2023, 32% of transfer operations prove to be successful out of the initial 24,334 transactions, while 11% result in failed transfers.
+
+
+
+The number of transfers, transactions, and modifications exhibits a consistent upward trend, steadily increasing on a month-to-month basis.
+
+
+The percentage of successful internal transfer conversions is on an upward trajectory, averaging at 37%. It's worth noting that the percentage was significantly lower in the first two months, impacting the overall average. Conversely, the percentage of failed internal transfer conversions hovers around 3%.
+
