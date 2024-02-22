f82e5ef9b668ff36fe8e7af8a16506f40063fa49
othososs <103260323+othososs@users.noreply.github.com>
2024-02-22 12:11:18 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,1 +1,33 @@
-h
+Acquisition :
+
+Taux de conversion par canal d'acquisition (publicité en ligne, référencement, recommandation, etc.).
+Analyse des leads qualifiés.
+Temps moyen entre la première interaction et la conversion.
+Analyse de l'efficacité des campagnes publicitaires spécifiques.
+Client Engagement :
+
+Fréquence d'utilisation de chaque fonctionnalité spécifique du service.
+Temps moyen passé sur la plateforme par session.
+Analyse de la navigation sur le site ou l'application.
+Nombre moyen de produits ou services utilisés par client.
+Client Satisfaction :
+
+Scores de satisfaction pour des aspects spécifiques tels que la facilité d'utilisation, la disponibilité du service client, etc.
+Comparaison de la satisfaction entre les segments de clients (nouveaux clients, clients fidèles, etc.).
+Analyse des commentaires clients pour identifier les tendances et les préoccupations récurrentes.
+Promotions :
+
+Suivi de la performance des promotions par canal.
+Taux de conversion après utilisation d'un code promotionnel.
+Analyse de la rétention des clients ayant utilisé une promotion.
+Churn Analysis :
+
+Taux de désabonnement par période.
+Raisons spécifiques du désabonnement.
+Comparaison du comportement des clients avant et après le désabonnement.
+Autres paramètres :
+
+Taux d'adoption des nouvelles fonctionnalités.
+Taux d'erreur lors des transactions.
+Analyse des sessions de service client en ligne.
+Taux d'utilisation des services bancaires mobiles.
