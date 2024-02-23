d314731c181c88595ade15db5008eaa982f5bf70
othososs <103260323+othososs@users.noreply.github.com>
2024-02-23 09:55:02 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,1 +1,45 @@
-ds
+Fréquence des Transactions :
+
+Nombre moyen de transactions par client sur une période donnée.
+Répartition des transactions par type (virements, paiements, consultation de solde, etc.).
+Utilisation des Services en Ligne :
+
+Taux d'adoption des services bancaires en ligne.
+Fréquence de connexion aux services en ligne par client.
+Temps Passé sur la Plateforme :
+
+Temps moyen que les clients passent sur la plateforme par session.
+Tendances de la durée des sessions au fil du temps.
+Nombre de Produits/Services Utilisés :
+
+Profil de diversification des produits par client.
+Taux d'adoption de nouveaux produits ou services.
+Réponses aux Campagnes d'Engagement :
+
+Taux de participation aux campagnes promotionnelles ou d'engagement.
+Analyse des résultats des campagnes pour mesurer l'impact sur l'engagement.
+Satisfaction Après Interaction :
+
+Scores de satisfaction recueillis après des interactions spécifiques (par exemple, après un appel au service client, une transaction en ligne, etc.).
+Utilisation des Fonctionnalités Spécifiques :
+
+Fréquence d'utilisation de fonctionnalités spécifiques, telles que la gestion des alertes, la configuration de virements automatiques, etc.
+Navigation sur le Site/ l'Application :
+
+Chemin de navigation typique des clients lorsqu'ils utilisent votre site web ou votre application.
+Taux d'abandon à différentes étapes du processus en ligne.
+Réponses aux Enquêtes de Satisfaction :
+
+Taux de participation aux enquêtes de satisfaction client.
+Résultats des enquêtes pour évaluer la perception de l'expérience client.
+Activité sur les Médias Sociaux :
+
+Engagement sur les plateformes de médias sociaux liées à votre service bancaire.
+Suivi des commentaires, mentions, ou questions des clients sur les médias sociaux.
+Taux de Conversion après Interaction :
+
+Mesurez le pourcentage de clients ayant effectué une action souhaitée après une interaction spécifique (par exemple, ouverture d'un compte après une consultation en ligne).
+Analyse des Taux d'Utilisation des Applications Mobiles :
+
+Pourcentage de clients utilisant activement les applications mobiles pour accéder aux services bancaires.
+Fréquence d'utilisation des fonctionnalités spécifiques de l'application mobile.
