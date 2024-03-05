eab8d3053ae804985446ff9fdebf0a7817fcf4b2
othososs <103260323+othososs@users.noreply.github.com>
2024-03-05 15:46:35 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,1 +1,52 @@
-Je vais jouer un rôle essentiel au sein du département Personal Finance GBL Customer Experience, Applied Innovation et Marketing de BNP Paribas Personal Finance. Mon travail sera centré sur l'exploitation des données dans les pays PF pour identifier des opportunités liées à la Banque digitale et à des sujets innovants comme LLM, GenAI, et OpenBanking. En tant que membre de l'équipe Business Performance, je mènerai des analyses de données, partagerai mes résultats avec les parties prenantes, et interagirai activement avec les équipes analytiques et digitales à l'échelle internationale, contribuant ainsi à l'optimisation de l'expérience client et à l'innovation digitale.
+omepage:
+Conversion Rate:
+
+Conversion to opening a bank account.
+Conversion to requesting specific products (credit cards, loans, etc.).
+Conversion to using online tools (financial calculators, simulators).
+Bounce Rate:
+
+Bounce rate for visitors from different channels (organic search, paid ads, etc.).
+Average Time Spent on Page:
+
+Average time spent by new visitors on key sections (services, benefits, security).
+Click-through Rate:
+
+Click-through rate on main action buttons (Open an Account, Explore Our Offers, etc.).
+Click-through rate on crucial information sections (Fees and Rates, Account Security).
+Engagement Rate:
+
+Engagement rate with interactive elements (simulators, financial tools).
+Engagement rate with educational content (financial tips, informative videos).
+Landing Page:
+Conversion Rate:
+
+Conversion to opening a bank account.
+Conversion to requesting additional information.
+Bounce Rate of the Page:
+
+Bounce rate for different traffic segments (new visitors, returning visitors).
+Conversion Rate per Click:
+
+Conversion for visitors who clicked on specific offers (credit cards, loans).
+Conversion for visitors who clicked on security information.
+Engagement Rate:
+
+Engagement rate with account opening forms.
+Engagement rate with trust-building elements (customer testimonials, security certifications).
+Funnel:
+Conversion Rate by Stage:
+
+Conversion from the first stage (visit) to information request.
+Conversion from information request to account opening.
+Abandonment Rate by Stage:
+
+Abandonment at the personal data validation page.
+Abandonment at the account opening confirmation.
+Average Time to Traverse the Funnel:
+
+Average time to move from information request to account opening.
+Average time to complete the account opening confirmation.
+Overall Conversion Rate:
+
+Overall conversion from visit to account opening.
