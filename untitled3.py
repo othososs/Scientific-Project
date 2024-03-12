de0e55c45c24e8ab1b15039787f8f6c44076650f
othososs <103260323+othososs@users.noreply.github.com>
2024-03-12 14:32:52 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,5 +1,1 @@
-Salut Claudie,
-
-J'espère que tu vas bien. J'ai apporté les modifications nécessaires sur Adobe Analytics, mais je rencontre un problème avec le filtre de la page de destination. Les résultats dans "onboarding step" ne me semblent pas logiques. En revanche, pour "saving account", les résultats sont cohérents. Voici le lien vers le tableau de bord. Si tu es disponible, nous pouvons passer en revue ces résultats ensemble.
-
-Bien à toi,
+kd
