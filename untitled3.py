1520824b215cfdf2b54de11129584f75cb9cc28f
othososs <103260323+othososs@users.noreply.github.com>
2024-03-18 00:16:05 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,1 +1,5 @@
-kd
+Regarding competitors like Cetelem, Franfinance, Sofinco, and Cofidis display similar types of windows as Cetelem, showcasing simulations of credit amounts with various types of credit requests.
+
+For Cetelem Spain, to view the different types of credit requests, one needs to click on the "Prestamos" tab instead of displaying them directly on the homepage, which could potentially impact first-time visitors to the site.
+
+I also explored competitors in other markets such as the United States, where credit consumption is high. I visited a few websites like Prosper, Avant, Upgrade, and SoFi. They all share a common feature: a creatively designed display page where different products are clearly indicated with ranges of credit amounts that users can request. Additionally, the first step of registration only requires simple data such as email, name, and surname.
