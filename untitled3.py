71315d20dc77ad0ac1e4045a62443ca491c56bb2
othososs <103260323+othososs@users.noreply.github.com>
2024-03-21 09:58:50 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,3 +1,7 @@
+Why are the volumes low? Are there other sources that are not tracked on Adobe Analytics?
+
+The sum of the amounts between deposit and savings does not match the total.
+
 To monitor various metrics during A/B testing, we have developed a dashboard that is constantly being improved. I am going to present its current version to you, and we would be delighted to receive your feedback on potential enhancements.
 
 This dashboard is structured around three funnels: deposit, savings, and landing page. It allows the visualization of the conversion rate as well as the number of visitors at each step of the onboarding process. It also provides other metrics such as region, device type, and visualizations on visitors returning to complete the process.
