5086bcb43500eb0d07ed905f210640e897280325
othososs <103260323+othososs@users.noreply.github.com>
2024-03-21 09:58:32 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,1 +1,5 @@
-cx
+To monitor various metrics during A/B testing, we have developed a dashboard that is constantly being improved. I am going to present its current version to you, and we would be delighted to receive your feedback on potential enhancements.
+
+This dashboard is structured around three funnels: deposit, savings, and landing page. It allows the visualization of the conversion rate as well as the number of visitors at each step of the onboarding process. It also provides other metrics such as region, device type, and visualizations on visitors returning to complete the process.
+
+Similarly, for the other funnels, the last one, the landing page funnel, presents the results in a global manner to compare the effectiveness of each landing page.
