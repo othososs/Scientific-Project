863d56614851c27c9aa5f8b6b1ff88ced7687808
othososs <103260323+othososs@users.noreply.github.com>
2024-03-26 21:57:56 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,12 +1,16 @@
-def get_exit_page_counts(df, entry_page, return_type):
-    filtered_df = df[(df['entry_page'] == entry_page) & (df['return'] == return_type)]
+def get_exit_page_counts(df, entry_pages, return_type):
+    filtered_df = df[(df['entry_page'].isin(entry_pages)) & (df['return'] == return_type)]
     exit_page_counts = filtered_df['exit_page'].value_counts()
     return exit_page_counts
 
 # Deposit
-df_exit_page_deposit_first = get_exit_page_counts(df, 'deposit', 'first')
-df_exit_page_deposit_second = get_exit_page_counts(df, 'deposit', 'second')
+df_exit_page_deposit_first = get_exit_page_counts(df, ['deposit'], 'first')
+df_exit_page_deposit_second = get_exit_page_counts(df, ['deposit'], 'second')
 
 # Saving
-df_exit_page_saving_first = get_exit_page_counts(df, 'saving', 'first')
-df_exit_page_saving_second = get_exit_page_counts(df, 'saving', 'second')
+df_exit_page_saving_first = get_exit_page_counts(df, ['saving'], 'first')
+df_exit_page_saving_second = get_exit_page_counts(df, ['saving'], 'second')
+
+# All (Deposit and Saving)
+df_exit_page_all_first = get_exit_page_counts(df, ['deposit', 'saving'], 'first')
+df_exit_page_all_second = get_exit_page_counts(df, ['deposit', 'saving'], 'second')
