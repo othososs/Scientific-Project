f7a5913fcebef912ffa36a362b47e45ac15cd9ec
othososs <103260323+othososs@users.noreply.github.com>
2024-03-27 23:10:07 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,1 +1,4 @@
-result = result.rename(columns={'exit_page': 'Pagename', 'entry_db_journey_page': 'Session_type'})
+# Supposons que 'df' est votre DataFrame
+df['min_session_number'] = df.groupby('mk_id')['session_number'].transform('min')
+df['Return_visitors'] = df.apply(lambda row: 'First visit' if row['session_number'] == row['min_session_number'] else 'Returning visitor', axis=1)
+df = df.drop('min_session_number', axis=1)  # Supprimer la colonne temporaire
