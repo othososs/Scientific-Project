cea7f388f762109c35e55b18127edb087c9cd318
othososs <103260323+othososs@users.noreply.github.com>
2024-03-27 22:20:19 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,1 +1,23 @@
-ormally, we said that the percentage is the ratio between the number of exits on a page and the total number of exits. It's normal that for ALL, it would be 1 because it considers both deposits and savings. Here's how I calculate it. If there's any mistake, please let me know so I can correct it
+import pandas as pd
+
+# Création du DataFrame initial
+data = {'entry_db_journey_page': ['Saving account', 'Saving account', 'Saving account', 'Deposits', 'Deposits', 'Deposits', 'Deposits'],
+        'exit_page': ['google.com', 'instagram.com', 'microsoft.com', 'google.com', 'instagram.com','microsoft.com','google.com'],
+        'session_number': [2, 4, 2, 1, 6, 3 ,7],
+        'returning_visitor': [1,0 ,0 ,1 ,0 ,1 ,0],
+        "mk_id": [23231 ,23948 ,43212 ,45642 ,2143 ,43232 ,3436]}
+
+df = pd.DataFrame(data)
+
+# Calculer le nombre et le pourcentage des pages de sortie par type d'entrée
+result = df.groupby(['entry_db_journey_page','exit_page']).size().reset_index(name='Number')
+result['Percentage'] = result.groupby('entry_db_journey_page')['Number'].apply(lambda x: x / float(df['exit_page'].nunique()))
+
+# Ajouter les données pour "All"
+all_data = df.groupby('exit_page').size().reset_index(name='Number')
+all_data['Percentage'] = all_data['Number'] / df['exit_page'].count()
+all_data['entry_db_journey_page'] = "All"
+
+result = pd.concat([result[['exit_page','Number','Percentage','entry_db_journey_page']], all_data[['exit_page','Number','Percentage','entry_db_journey_page']]])
+
+print(result)
