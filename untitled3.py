b97c149261e458e921fd15c159148b9039731584
othososs <103260323+othososs@users.noreply.github.com>
2024-03-27 16:28:56 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,1 +1,1 @@
-Just for confirmation, I remember Claudie confirmed that we will consider both returning and first visitors, so there won't be any filter in this part
+ormally, we said that the percentage is the ratio between the number of exits on a page and the total number of exits. It's normal that for ALL, it would be 1 because it considers both deposits and savings. Here's how I calculate it. If there's any mistake, please let me know so I can correct it
