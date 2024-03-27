e674c1a5c43efcfe3312279b4d3346ef167dcf1a
othososs <103260323+othososs@users.noreply.github.com>
2024-03-27 15:22:33 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,5 +1,1 @@
-"Hey, I have a few questions:
-
-1- When you mention merging sessions and the grouped dataframe, is it necessary to do this to obtain the link or for another reason? Because I have been working with df_sessions. If there's a reason I'm not aware of, please let me know.
-
-2- A lower value for session_number means that I set 1 as a reference for the initial visits of the visitors
+Okay, I understand returning visitors better now, it's clearer. I'll make a few changes and send you the file. Thanks for your feedback
