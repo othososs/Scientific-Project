02e59dac8467c9a4dcbd0809b433d7009fa36f08
othososs <103260323+othososs@users.noreply.github.com>
2024-03-28 00:08:24 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,4 +1,16 @@
-# Supposons que 'df' est votre DataFrame
-df['min_session_number'] = df.groupby('mk_id')['session_number'].transform('min')
-df['Return_visitors'] = df.apply(lambda row: 'First visit' if row['session_number'] == row['min_session_number'] else 'Returning visitor', axis=1)
-df = df.drop('min_session_number', axis=1)  # Supprimer la colonne temporaire
+
+# Pour la première partie (visiteurs 'First Visit')
+first_visits = df[(df['returning_visitor'].str.lower() == 'first visit') & df['entry_db_journey_page'].isin(['Deposits', 'Saving account'])]
+
+first_visits_grouped = first_visits.groupby(['entry_db_journey_page', 'exit_page']).size().reset_index(name='Number_of_visitors')
+print("Première partie :\n", first_visits_grouped)
+
+# Pour la deuxième partie (visiteurs 'Returning visitor')
+# Obtenez les mk_id des visiteurs 'First Visit'
+first_visit_mk_ids = first_visits['mk_id'].unique()
+
+# Filtrer les visites ultérieures de ces visiteurs
+returning_visits = df[df['mk_id'].isin(first_visit_mk_ids) & (df['returning_visitor'].str.lower() == 'returning visitor')]
+
+returning_visits_grouped = returning_visits.groupby(['entry_db_journey_page', 'exit_page']).size().reset_index(name='Number_of_visitors')
+print("Deuxième partie :\n", returning_visits_grouped)
