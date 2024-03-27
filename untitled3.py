6055eaee89ecf45f364f957fb367c883910445d0
othososs <103260323+othososs@users.noreply.github.com>
2024-03-27 15:07:18 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,1 +1,5 @@
-Hey Catarina, I have a question. Regarding the percentage in the exit page table, normally it's the ratio between the number of exits from a site and the total number of exits. For the total number of exits, do you only consider deposits and savings or is it the sum of everything?
+"Hey, I have a few questions:
+
+1- When you mention merging sessions and the grouped dataframe, is it necessary to do this to obtain the link or for another reason? Because I have been working with df_sessions. If there's a reason I'm not aware of, please let me know.
+
+2- A lower value for session_number means that I set 1 as a reference for the initial visits of the visitors
