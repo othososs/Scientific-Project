97f5c177dd3e49816aca49c72bb597d882d1b58c
othososs <103260323+othososs@users.noreply.github.com>
2024-03-28 11:27:36 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,1 +1,1 @@
-result['Percentage'] = result.groupby('entry_db_journey_page')['Number'].apply(lambda x: x / float(df[df['entry_db_journey_page'] == x.name]['exit_page'].nunique()))
+result['Percentage'] = result.groupby('entry_db_journey_page')['Number'].apply(lambda x: x / x.sum())
