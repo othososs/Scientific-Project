6aaa73564db158d2fe002fef8b2e269df30b2450
othososs <103260323+othososs@users.noreply.github.com>
2024-03-29 15:36:35 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,3 +1,3 @@
-Hello,
+Bonjour,
 
-I finished the top exit page file and imported it into Power BI. It works, but we can't see the entire URL link. If you have time, could you please take a look to see if there's a way to optimize the space on the dashboard? Additionally, there's another issue: when we don't choose any filters, instead of getting all results, we get another result.
+Non, je ne suis pas au bureau aujourd'hui. Claudie m'a parlé à propos de quelque chose en relation avec SAS, SQL. Pour avoir plus d'informations, j'ai un point avec Chaima et Ricardo maintenant.
