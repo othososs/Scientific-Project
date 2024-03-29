4b3f55a0c1e0ed46ce4ff1c65bf3b9543899d7c4
othososs <103260323+othososs@users.noreply.github.com>
2024-03-29 16:15:34 +0100

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,3 +1,1 @@
-Bonjour,
-
-Non, je ne suis pas au bureau aujourd'hui. Claudie m'a parlé à propos de quelque chose en relation avec SAS, SQL. Pour avoir plus d'informations, j'ai un point avec Chaima et Ricardo maintenant.
+Juste aujourd'hui, je suis un peu loin de Paris, en île de France, et du coup je n'allais pas arriver à l'heure.
