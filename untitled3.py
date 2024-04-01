63912308d9df1d1044d59c4ce56da311c7176620
othososs <103260323+othososs@users.noreply.github.com>
2024-04-01 23:05:12 +0200

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,1 +1,7 @@
-Juste aujourd'hui, je suis un peu loin de Paris, en île de France, et du coup je n'allais pas arriver à l'heure.
+Hello Claudie,
+
+J'espère que tu as passé un bon weekend.
+
+En ce qui concerne l'extraction, j'ai utilisé les données que Catarina m'a partagées sur Sparrow. La plage de dates est entre le 27 février 2024 et le 12 mars 2024.
+
+De plus, concernant le tableau de bord sur Adobe, j'ai apporté quelques modifications en ajoutant l'étape 0. Cependant, j'ai remarqué un volume non cohérent pour le compte d'épargne. Nous pourrons examiner ensemble cela pour trouver une explication.
