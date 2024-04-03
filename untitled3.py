9cc767a502543bc48a32665db0007d4cc86e5acb
othososs <103260323+othososs@users.noreply.github.com>
2024-04-03 16:28:36 +0200

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,1 +1,8 @@
-d
+Hello,
+
+I hope you're doing well. I would like to know your opinion on some results we found in our Adobe Analytics analysis. For the savings account, we compare visitors who have selected a plan (either standard or premium) and we take the sum. Then, we compare it with the number of visitors who were directed to the identification page, but we notice that we lose more than 60% of visitors, which doesn't make sense because after selecting the plan, they should be directed straight to identification.
+Do you have any idea about this?
+
+Here is a screenshot of the results table on Adobe:
+
+Kind regards
