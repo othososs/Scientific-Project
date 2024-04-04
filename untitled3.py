291999bb649a8115c1494457606ce3fce7458a5d
othososs <103260323+othososs@users.noreply.github.com>
2024-04-04 17:08:01 +0200

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,1 +1,8 @@
-d
+Hello everyone, please find in the Excel file 'fichier.xlsx' the analysis results for volumes (deposit and saving accounts) and the calculation of conversion rates. The following sheets are included:
+
+- **Graphs**: Contains visual representations of volume and conversion rates.
+- **Volume and Conversion**: You will find a table representing the volumes (homepage, deposit, saving accounts) as well as the corresponding conversion rates. The calculation method is indicated in the column titles. The date range covered is from 25/02/2024 to 04/04/2024.
+- **Traffic Source**: There are two tables (saving, sdeposit) representing the number of visitors we have had from each marketing channel.
+
+If you have any questions, feel free to contact me.
+
