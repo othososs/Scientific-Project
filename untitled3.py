89ed81a1447bda04eb39b0fc97535ce8f0c6a6cf
othososs <103260323+othososs@users.noreply.github.com>
2024-04-12 16:25:51 +0200

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,7 +1,1 @@
-Bonjour,
-
-Je vous remercie pour votre rappel. Malheureusement, pour l'instant je n'ai pas reçu mon nouveau titre de séjour. J'ai contacté la préfecture à plusieurs reprises, et ils m'ont informé que je recevrai une attestation de prolongation après l'expiration de mon titre actuel, afin de pouvoir poursuivre mon stage dans des conditions légales. J'espère donc recevoir un document dans les jours à venir.
-
-Par ailleurs, j'aimerais savoir s'il y aura une suspension de mon stage, et quelle serait la durée de cette suspension le cas échéant ?
-
-Cordialement
+c
