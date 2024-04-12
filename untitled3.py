f69b9896d5246cf91285e7b06eff2ea66fdd07ca
othososs <103260323+othososs@users.noreply.github.com>
2024-04-12 15:45:54 +0200

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,5 +1,7 @@
-Hello everyone,
+Bonjour,
 
-I hope you are doing well. You will find the file analyzing volumes, conversion rate, and volume by source.
+Je vous remercie pour votre rappel. Malheureusement, pour l'instant je n'ai pas reçu mon nouveau titre de séjour. J'ai contacté la préfecture à plusieurs reprises, et ils m'ont informé que je recevrai une attestation de prolongation après l'expiration de mon titre actuel, afin de pouvoir poursuivre mon stage dans des conditions légales. J'espère donc recevoir un document dans les jours à venir.
 
-I noticed that after changing the segments, we have a significant increase in volumes for deposit as well as the conversion rate.
+Par ailleurs, j'aimerais savoir s'il y aura une suspension de mon stage, et quelle serait la durée de cette suspension le cas échéant ?
+
+Cordialement
