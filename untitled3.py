360eb87f6cf0fadb946ea59684fc3b387880072a
othososs <103260323+othososs@users.noreply.github.com>
2024-05-22 16:12:07 +0200

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,9 +1,1 @@
-Thank you everyone for being here.
-
-Today, I will present the weekly reporting to show the different variations in the Sharo dashboard indicators. Every weekend, you will receive a report that summarizes the overall results on three main symbols.
-
-The key variables will be fixed, and if there are variations in the others, we will add them to this box to reflect the latest changes.
-
-In this box, we will display the conversion rate with a small note and the calculation method.
-
-This * represents the variation compared to the average of the last three weeks.
+Here is the presentation with the necessary modifications. Please check if everything is in order so that you can transfer the report to Alberto and his team for their feedback. In any case, there will surely be other suggestions from them to improve the report
