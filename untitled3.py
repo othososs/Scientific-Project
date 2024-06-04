e8637b95a95bcf06d89b1541be00ce19181b1344
othososs <103260323+othososs@users.noreply.github.com>
2024-06-04 17:46:33 +0200

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,5 +1,5 @@
-We observe a decrease in the number of visitors who started the registration process and those who completed it compared to last week and the average of the past three weeks. However, there is a significant reduction in errors, which is generally positive. Additionally, regarding the conversion rate, there is a decrease of 3.2% compared to last week. Overall, for Civil Account, there is a decrease in the number of visitors who started the process, but the number of visitors on the Landing Page has generally increased.
-
-Slide 2
-We observe a significant increase in deposits compared to last week. Consequently, the conversion rate has also increased by 1.04%.
-
+Hello Ana,
+I hope you're doing well.
+I have sent last week's report. For now, we have kept the same version, awaiting your comments and suggestions to see how to improve it.
+Have a nice evening.
+Regards
