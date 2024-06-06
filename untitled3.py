cfe556733f9fdb7a99e7d37063b169ed7622b6d0
othososs <103260323+othososs@users.noreply.github.com>
2024-06-06 10:16:28 +0200

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -1,5 +1,29 @@
-Hello Ana,
-I hope you're doing well.
-I have sent last week's report. For now, we have kept the same version, awaiting your comments and suggestions to see how to improve it.
-Have a nice evening.
-Regards
+from imblearn.under_sampling import RandomUnderSampler
+
+# Separate features (X) and target variable (y)
+X = data.drop('target', axis=1)
+y = data['target']
+
+# Initialize the random undersampler
+rus = RandomUnderSampler(random_state=42)
+
+# Perform random undersampling
+X_resampled, y_resampled = rus.fit_resample(X, y)
+
+# Print the new class distribution
+print('Resampled dataset shape:', Counter(y_resampled))
+
+from imblearn.over_sampling import MSMOTE
+
+# Separate features (X) and target variable (y)
+X = data.drop('target', axis=1)
+y = data['target']
+
+# Initialize MSMOTE
+msmote = MSMOTE(random_state=42)
+
+# Perform MSMOTE
+X_resampled, y_resampled = msmote.fit_resample(X, y)
+
+# Print the new class distribution
+print('Resampled dataset shape:', Counter(y_resampled))
